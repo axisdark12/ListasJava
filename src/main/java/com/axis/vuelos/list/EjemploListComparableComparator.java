366012b8edc9f016c4d6d8b678f807fef52d5f9f
usuario1193 <usuario1193@localhost>
2025-03-13 14:43:42 -0600

    cambios a todos los tipos de colecciones

---
+++ src/main/java/com/axis/vuelos/list/EjemploListComparableComparator.java
@@ -0,0 +1,31 @@
+package org.aespinosa.ejemplos.list;
+
+import org.aespinosa.ejemplos.modelo.Alumno;
+
+import java.util.*;
+import static java.util.Comparator.comparing;
+
+public class EjemploListComparableComparator {
+    public static void main(String[] args) {
+        List<Alumno> sa = new LinkedList<>();
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Luci", 4));
+        sa.add(new Alumno("Jano", 7));
+        sa.add(new Alumno("Andres", 3));
+        sa.add(new Alumno("Zeus2", 2));
+        sa.add(new Alumno("Zeus", 2));
+        sa.add(new Alumno("Lucas", 2));
+        sa.add(new Alumno("Lucas", 3));
+        //ordenamiento mayor a menor
+        //Collections.sort(sa, ( a,b) -> b.getNota().compareTo(a.getNota()));
+        //ordenamiento menor a mayor
+        //sa.sort((a, b) -> a.getNota().compareTo(b.getNota()));
+        sa.sort(comparing(Alumno::getNombre).reversed());
+        System.out.println(sa);
+
+        System.out.println("Iterando usando Stream forEach");
+
+        sa.forEach(System.out::println);
+    }
+}
